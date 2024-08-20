047e69d1e801e0103786d3d0248cc7c429391195
RythenGlyth <42944106+RythenGlyth@users.noreply.github.com>
2024-08-20 19:07:00 +0200

    Add downloading of unifont in init.bat

--- init.bat
+++ init.bat
@@ -2,6 +2,7 @@
 
 REM Install chocolatey via official command from their website
 powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+powershell -Command "Invoke-WebRequest http://www.unifoundry.com/pub/unifont/unifont-15.0.01/font-builds/unifont-15.0.01.ttf -OutFile unifont-15.0.01.ttf"
 
 choco install ffmpeg -y
 choco install nodejs -y
