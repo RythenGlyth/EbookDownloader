8efa056de4e31a6a085810906ecb9a373046b916
RythenGlyth <rythenglyth@gmail.com>
2024-08-30 17:04:42 +0200

    change init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ wget "http://www.unifoundry.com/pub/unifont/unifont-15.0.01/font-builds/unifont-
 sudo apt install imagemagick
 sudo apt install ffmpeg
 sudo apt install nodejs npm
-sudo apt install mupdf
+sudo apt install mupdf-tools
 npm install
